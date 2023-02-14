4367e7d3c4338f43fe6b6df91266ac44730a7d1e
WobblyRunner <joshua.m.willis8@gmail.com>
2023-02-14 09:58:56 -0800

    Modified tests to accomodate for additional cases, to test some new behavior.

--- GenericsHomework.Tests/CircleLinkedListTests.cs
+++ GenericsHomework.Tests/CircleLinkedListTests.cs
@@ -1,3 +1,5 @@
+using System.Net.Security;
+
 namespace GenericsHomework.Tests
 {
 	[TestClass]
@@ -13,12 +15,12 @@ namespace GenericsHomework.Tests
 		{
 			const int value = 1337;
 			Node<int> root = new(value);
-			string rootToString;
+			string? rootToString;
 
 			rootToString = root.ToString();
 
-			Assert.IsNotNull(rootToString);
-			Assert.AreEqual<string>(value.ToString(), rootToString);
+			Assert.IsNotNull(rootToString, "ToString result is null");
+			Assert.AreEqual<string>(value.ToString(), rootToString, "ToString result is not the same result as the node's value");
 		}
 
 		[TestMethod]
@@ -60,7 +62,7 @@ namespace GenericsHomework.Tests
 		}
 
 		[TestMethod]
-		public void Node_Append_ThrowsArgumentExceptionOnDuplicate()
+		public void Node_Append_ThrowsArgumentExceptionOnDuplicateNotRoot()
 		{
 			Node<int> root = new(0);
 			const int value = 42;
@@ -71,27 +73,91 @@ namespace GenericsHomework.Tests
 		}
 
 		[TestMethod]
-		public void Node_Clear_NextReferenceIsLost()
+		public void Node_Append_ThrowsArgumentExceptionOnDuplicateRootValue()
+		{
+			const int value = 100;
+			Node<int> root = new(value);
+
+			Assert.ThrowsException<ArgumentException>(() => root.Append(value));
+		}
+
+		[TestMethod]
+		public void Node_AppendRange_ReturnsAllAddedNodes()
 		{
 			Node<int> root = new(0);
-			const int value = 25;
-			Node<int> next, nextAfterClear;
+			const int value1 = 5, value2 = 10, value3 = 25;
+			int[] values = { value1, value2, value3 };
+			Node<int>[] appended;
+			int[] appendedValueCast;
+			
+			appended = root.Append(values);
+			appendedValueCast = appended.Select(node => node.Value).ToArray();
 
-			root.Append(value);
-			next = root.Next;
+			CollectionAssert.AreEquivalent(appendedValueCast, values);
+		}
+
+		[TestMethod]
+		public void Node_AppendRange_ReturnsNodesInOrder()
+		{
+			Node<int> root = new(0);
+			const int value1 = 5, value2 = 10, value3 = 25;
+			int[] values = { value1, value2, value3 };
+			Node<int>[] appended;
+			
+			appended = root.Append(values);
+
+			Assert.AreEqual<int>(appended[0].Value, value1);
+			Assert.AreEqual<int>(appended[1].Value, value2);
+			Assert.AreEqual<int>(appended[2].Value, value3);
+		}
+
+		[TestMethod]
+		public void Node_AppendRange_NoOperationOnEmpty()
+		{
+			Node<int> root = new(0);
+
+			root.Append(Array.Empty<int>());
+
+			Assert.AreEqual<Node<int>>(root, root.Next);
+		}
+
+		[TestMethod]
+		public void Node_AppendRange_ThrowsArgumentExceptionOnDuplicate()
+		{
+			Node<int> root = new(0);
+			const int value1 = 5, value2 = 5, value3 = 5;
+			int[] values = { value1, value2, value3 };
+
+			Assert.ThrowsException<ArgumentException>(() => root.Append(values));
+		}
+
+		[TestMethod]
+		public void Node_Clear_NextReferenceIsLostForAll()
+		{
+			/* In a circular linked list, we can practically treat any node as the root node as the collection is inherently rootless.
+			 *	In this case, each node can effectively be treated an encapsulation of a container for the entire list.
+			 *	This means that when we Clear() the list, developers will expect that entire list to become destroyed and each node orphanized. */
+
+			Node<int> root = new(0);
+			Node<int> next1, next2, next3;
+
+			root.Append(1, 2, 3);
+			next1 = root.Next;
+			next2 = root.Next.Next;
+			next3 = root.Next.Next.Next;
 			root.Clear();
-			nextAfterClear = root.Next;
 
-			Assert.AreNotEqual<Node<int>>(next, nextAfterClear);
-			Assert.AreEqual<Node<int>>(root, nextAfterClear);
-			Assert.IsNotNull(nextAfterClear);
+			Assert.AreEqual(root, root.Next);
+			Assert.AreEqual(next1, next1.Next);
+			Assert.AreEqual(next2, next2.Next);
+			Assert.AreEqual(next3, next3.Next);
 		}
 
 		[TestMethod]
 		public void Node_Exists_ReturnsTrueOnExists()
 		{
 			Node<int> root = new(0);
-			ReadOnlySpan<int> values = stackalloc int[3] { 1, 2, 3 };
+			int[] values = { 1, 2, 3 };
 			bool exists = false;
 
 			root.Append(values);
@@ -103,7 +169,7 @@ namespace GenericsHomework.Tests
 		public void Node_Exists_ReturnsFalseOnNotExists()
 		{
 			Node<int> root = new(0);
-			ReadOnlySpan<int> values = stackalloc int[3] { 4, 5, 6 };
+			int[] values = { 4, 5, 6 };
 			bool exists = true;
 			const int notExistingValue = 42;
 
